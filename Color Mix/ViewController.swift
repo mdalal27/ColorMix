c42d39d530e35892a4fe392d3af616ebaba79467
mdalal27 <60400079+mdalal27@users.noreply.github.com>
2020-02-10 12:00:56 -0500

    Added Sliders
    This is Moiz's ColorMix Yay

--- Color Mix/ViewController.swift
+++ Color Mix/ViewController.swift
@@ -1,10 +1,7 @@
 //
 //  ViewController.swift
 //  Color Mix
-//
-//  Created by WoLf, ChRiStOpHeR (LYHS) on 1/31/20.
-//  Copyright © 2020 Ava Wilson . All rights reserved.
-//
+
 
 import UIKit
 
@@ -16,19 +13,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var blueSwitch: UISwitch!
     @IBOutlet weak var greenSwitch: UISwitch!
     
+    @IBOutlet weak var redSlider: UISlider!
+    
+    @IBOutlet weak var greenSlider: UISlider!
+    
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setColor()
     }
 
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        setColor()
+    }
     @IBAction func switchChanged(_ sender: UISwitch){
         setColor()
     }
     
     func setColor() {
-        let red: CGFloat = redSwitch.isOn ? 1:0
-        let green: CGFloat = greenSwitch.isOn ? 1:0
-        let blue: CGFloat = blueSwitch.isOn ? 1:0
+        let red: CGFloat = redSwitch.isOn ? CGFloat(redSlider.value) : 0
+        let green: CGFloat = greenSwitch.isOn ? CGFloat(greenSlider.value) : 0
+        
+        let blue: CGFloat = blueSwitch.isOn ? CGFloat(blueSlider.value) : 0
         let color = UIColor(red: red, green: green, blue: blue, alpha: 0.49)
         colorView.backgroundColor = color
     }
